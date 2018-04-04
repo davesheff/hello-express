40a7377dc712ba19403a03737b18805a716bb3ef
David Sheffield <davesheff@live.co.uk>
2018-04-04 09:14:16 +0100

    =Post request added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const helloWorld = require('./controllers/helloWorld');
 const bodyParser = require('body-parser');
+const createShoppingList = require('./controllers/createShoppingList');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', helloWorld);
+
+app.post('/shopping-lists', createShoppingList);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- controllers/createShoppingList.js
+++ controllers/createShoppingList.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 function createShoppingList(req, res) {
+  const filename = Date.now().toString();
+  const contents = JSON.stringify(req.body);
+
+  fs.writeFile(path.join(__dirname, 'shoppingLists', filename), contents, (err) => {
+    if (err) throw err;
+    res.send({ filename: filename });
+  });
 }
 
 module.exports = createShoppingList;
