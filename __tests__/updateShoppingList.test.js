2a660eedceaecf12aa5edbe057a32d90585d2cfd
David Sheffield <davesheff@live.co.uk>
2018-04-24 14:13:51 +0100

    Step 6 testing a PUT route

--- __tests__/updateShoppingList.test.js
+++ __tests__/updateShoppingList.test.js
@@ -5,14 +5,31 @@ const path = require('path');
 const httpMocks = require('node-mocks-http');
 const updateShoppingList = require('../controllers/updateShoppingList');
 
-it('creates a new shopping list', (done) => {
+it('updates an existing shopping list', (done) => {
   expect.assertions(1);
   const filename = Date.now().toString();
-  const filePath = path.join(__dirname, '../controllers/shoppingLists', filename);  
+  const filePath = path.join(__dirname, '../controllers/shoppingLists', filename);
   const body = {
-    items: ['broccoli', 'bread', 'bananas'],
+    items: ['carrots', 'crunchies', 'cornflakes']
   };
   fs.writeFile(filePath, JSON.stringify(body), (err) => {
-    
-  })
+    const request = httpMocks.createRequest({
+      method: 'PUT',
+      url: '/shopping-lists/:filename',
+      params: {
+        filename: filename
+      },
+      body: body
+    });
+    const response = httpMocks.createResponse({
+      eventEmitter: require('events').EventEmitter,
+    });
+    updateShoppingList(request, response);
+    response.on('end', () => {
+      fs.readFile(filePath, 'utf8', (error, data) => {
+        expect(data).toBe(JSON.stringify(request.body));
+        done();
+      });
+    });
+  });
 });
